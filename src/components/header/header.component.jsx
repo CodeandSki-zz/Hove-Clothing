a140e804a1f815fd44c2b8d22e76758abfed7788
Andrew Hove <andrewhove@Andrews-MacBook-Pro.local>
2020-02-27 18:51:18 -0700

    added a bunch of functionality, including cart dropdown functionality

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,10 +3,13 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
+import Cart from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo'></Logo>
@@ -18,12 +21,18 @@ const Header = ({currentUser}) => (
                 currentUser ? <div className='option' onClick={() => auth.signOut()}>Sign Out</div> 
                 : <Link className='option' to='/signin'>Sign In</Link>
             }
+            <CartIcon />
         </div>
+        {
+            hidden ? null :
+        <CartDropdown/>
+        }
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 
 })
 
